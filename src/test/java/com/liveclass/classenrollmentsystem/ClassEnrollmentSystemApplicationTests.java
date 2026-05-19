22b69996f68ea6db40c29ab010f42e82e98327d0
milkaisgoody <ehwhgod0712@gmail.com>
2026-05-20 04:14:41 +0900

    Initial commit

---
+++ src/test/java/com/liveclass/classenrollmentsystem/ClassEnrollmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveclass.classenrollmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassEnrollmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
